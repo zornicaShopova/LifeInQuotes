7cf6fa228b2ef0a47a750a23a975fba432f738bd
zornicaShopova <zori.shopova@gmail.com>
2021-01-26 22:52:30 +0200

    initial commit

---
+++ app/src/main/java/uni/fmi/masters/lq/MainActivity.java
@@ -0,0 +1,14 @@
+package uni.fmi.masters.lq;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
