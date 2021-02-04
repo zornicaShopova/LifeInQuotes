45156d64ab5f009dc0ede8299c914bdb44b2bbc3
zornicaShopova <zori.shopova@gmail.com>
2021-02-04 22:56:19 +0200

    Left navigation bar in Home page with general  changes menu for  photo,email,password and setting  like about for the app and logout;different icons

--- app/src/main/java/uni/fmi/masters/lq/HomeActivity.java
+++ app/src/main/java/uni/fmi/masters/lq/HomeActivity.java
@@ -1,14 +1,39 @@
 package uni.fmi.masters.lq;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class HomeActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    Toolbar toolbar;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer);
+        navigationView = findViewById(R.id.navigationView);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,
+             R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+
+        //hamburger symbol
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        actionBarDrawerToggle.syncState();
+
     }
 }
